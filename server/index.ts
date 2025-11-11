2780e147a7518da6ec15b1f45e920ca6da5bc370
World Bank <viv58490@gmail.com>
2025-11-11 13:44:32 +0000

    test

--- server/index.ts
+++ server/index.ts
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
   const port = 5000;
   server.listen({
     port,
-    host: "5000",
+    host: "127.0.0.1",
     reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
