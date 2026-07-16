1894264c4f222d50065188d77e1db696547a0e5b
World Bank <viv58490@gmail.com>
2026-07-16 02:16:23 +0000

    Deep scan round 19: Delete orphaned files - 37 stub packages, 13 dead server files, 5 dead lib files, 21 unused UI components, 3 dead pages, 4 temp files

--- packages/constants/index.ts
+++
@@ -1,7 +0,0 @@
-export const packageName = 'constants';
-export const API_BASE_URL = '/api';
-export const DEFAULT_CURRENCY = 'USD';
-export const MAX_TRANSFER_AMOUNT = 1000000;
-export const MIN_TRANSFER_AMOUNT = 0.01;
-export const PIN_LENGTH = 4;
-export const SESSION_TIMEOUT_MS = 60 * 60 * 1000;
